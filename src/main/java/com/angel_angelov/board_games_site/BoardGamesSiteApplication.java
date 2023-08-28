f390a309b21e3046cb17530e8bfe7a708ab782fe
Coolacho <sofiaangel35@gmail.com>
2023-08-28 18:17:07 +0300

    Initial commit

---
+++ src/main/java/com/angel_angelov/board_games_site/BoardGamesSiteApplication.java
@@ -0,0 +1,13 @@
+package com.angel_angelov.board_games_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGamesSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGamesSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angel_angelov/board_games_site/BoardGamesSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angel_angelov.board_games_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGamesSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
