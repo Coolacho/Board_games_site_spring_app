d0fc964f1e1d03b4cb38ebd93bf212b055e25869
Coolacho <sofiaangel35@gmail.com>
2023-08-29 23:36:51 +0300

    Added startup event for running the app

---
+++ src/main/java/com/angel_angelov/board_games_site/util/AppStartupEvent.java
@@ -0,0 +1,25 @@
+package com.angel_angelov.board_games_site.util;
+
+import com.angel_angelov.board_games_site.business.ProductService;
+import com.angel_angelov.board_games_site.data.product.Product;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final ProductService productService;
+
+    public AppStartupEvent(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        List<Product> products = this.productService.getProducts();
+        products.forEach(System.out::println);
+    }
+}
